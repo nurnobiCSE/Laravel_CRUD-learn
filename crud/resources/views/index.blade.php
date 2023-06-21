3767934fbf90d3278f24cc62a8d34348f494cbd7
nurnobiCSE <shuvo517942@gmail.com>
2023-06-21 13:47:44 +0600

    design change og tables

--- crud/resources/views/index.blade.php
+++ crud/resources/views/index.blade.php
@@ -74,7 +74,7 @@
               </tr>
               @endforeach
               @else
-                <tr><td>Data Not Found</td></tr>
+                <tr><td colspan="5"  class="text-center text-danger">Data Not Founds</td></tr>
               @endif
              
              
